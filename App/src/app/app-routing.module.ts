5fc30c45da38fc762269d9469777ac57eb483d06
ธนโชติ ศักดิ์ธรรมเจริญ <pet.14336@gmail.com>
2021-11-01 16:37:26 +0700

    Add Petch edit

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputEventComponent } from './components/event/input-event/input-event.component';
+import { ShowEventComponent } from './components/event/show-event/show-event.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {path : 'home' , component: HomeComponent},
+  {path : 'addEvent' , component : InputEventComponent},
+  {path : 'event' , component : ShowEventComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
